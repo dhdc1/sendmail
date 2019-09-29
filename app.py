0d666516c4fa5cdc0c9594336809160df6f1a4bd
dhdc1 <dhdctraining@gmail.com>
2019-09-29 08:02:18 +0700

    tehnn

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ import datetime
 from email.mime.text import MIMEText
 
 
-def send(to, q, do, place, hos):
+def send_mail(to, q, do, place, hos):
     msg = email.message.Message()
-
+    hos = hos.encode('utf-8').decode()
     number = ("แจ้งเตือน!!! หมายเลข {}".format(q)).encode('utf-8').decode()
     text = ("อีก 10 คิว จะถึงคิว {} ของท่าน กรุณาไปรอที่บริเวณ".format(do)).encode('utf-8').decode()
     place = place.encode('utf-8').decode()
-    body = """  <h1 style='color:blue'>{0}</h1>
-                <h2>{1} <u>{2}</u></h2> """.format(number, text, place)
+    body = """  <h1 style='color:green'>{0}</h1>
+                <h1 style='color:blue'>{1}</h1>
+                <h2>{2} <u>{3}</u></h2> """.format(hos, number, text, place)
     msg = MIMEText(body, 'html', 'utf-8')
 
     msg['Subject'] = Header(number, "utf-8")
@@ -34,4 +35,4 @@ def send(to, q, do, place, hos):
 
 
 if __name__ == '__main__':
-    send("tehnplk@gmail.com", "A107", "ซักประวัติ", "จุดซักประวัติ", "รพ.บางระจัน")
+    send_mail("tehnplk@gmail.com", "A109", "ซักประวัติ", "จุดซักประวัติ", "รพ.บางระจัน")
